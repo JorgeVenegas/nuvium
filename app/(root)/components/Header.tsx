111cd7f2d0744d0daf03db691300607a82b75def
Jorge Venegas <veljorge@Jorges-MacBook-Pro-3.local>
2024-12-11 19:17:55 -0600

    Set un Header main structure

--- app/(root)/components/Header.tsx
+++ app/(root)/components/Header.tsx
@@ -1,7 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      Search
+      <div className="header-wrapper">
+        FileUploader
+        <Button type="submit" className="sign-out-button">
+          <Image
+            src="/assets/icons/logout.svg"
+            alt="logo"
+            width={24}
+            height={24}
+            className="w-6"
+          />
+        </Button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
