db0ca935e8e6f46e8ec290cdf0f24ed2d647c1fb
Jorge Venegas <veljorge@Jorges-MacBook-Pro-3.local>
2025-01-03 18:47:25 -0600

    Updated identity to Nuvium

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const poppins = Poppins({
   variable: "--font-poppins",
 });
 export const metadata: Metadata = {
-  title: "StoreIt",
-  description: "StoreIt - The only storage solution you need.",
+  title: "Nuvium",
+  description: "Nuvium - The only storage solution you need.",
 };
 
 export default function RootLayout({
